f965116c505102be339f975e9e18723a9b52e491
alexHooper <alexmohack@gmail.com>
2018-08-11 13:09:02 +0530

    add url for our get_weather_ip view

--- display/urls.py
+++ display/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from .views import (
 	home,
+	get_weather_ip,
 )
 
 urlpatterns = [
 	path('', home, name='home'),
+	paht('get-weather-from-ip/', get_weather_ip, name='get_weather_ip'),
 ]
