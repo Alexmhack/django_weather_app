c1ad546a757b04aa0d3fa48cf776650e17f7124c
alexHooper <alexmohack@gmail.com>
2018-08-11 16:02:38 +0530

    add function for getting location from ip_address

--- display/views.py
+++ display/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import requests
 
 def home(request):
 	return render(request, "display/index.html")
+
+
+def get_location_from_ip(ip_address):
+	try:
+		response = requests.get("http://ip-api.com/json/{}").format(ip_addresss)
+		return response.json()
+	except Exception as e:
+		print(e)
 
 
 def get_weather_ip(request):
