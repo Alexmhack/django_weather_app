fc6e5fad1541f5e214f806ac409b12b1f2ad17f8
alexHooper <alexmohack@gmail.com>
2018-08-12 00:09:36 +0530

    print data for error detecting

--- display/views.py
+++ display/views.py
@@ -1,8 +1,9 @@
+import os
+import requests
+
 from django.shortcuts import render
 from django.http import JsonResponse
 
-import requests
-import os
 
 def home(request):
 	return render(request, "display/index.html")
@@ -17,7 +18,8 @@ def get_location_from_ip(ip_address):
 
 
 def get_weather_from_location(city, country_code):
-	token = os.environ.get("OPEN_WEATHER_TOKEN")
+	token = open(".env").read()
+	print(token)
 	url = f"https://api.openweathermap.org/data/2.5/weather?q={city},{country_code}&units=metric&appid={token}"
 	response = requests.get(url)
 	return response.json()
@@ -31,6 +33,7 @@ def get_weather_ip(request):
 		city = location.get("city")
 		country_code = location.get("countryCode")
 		weather_data = get_weather_from_location(city, country_code)
+		print(weather_data)
 		description = weather_data['weather'][0]['description']
 		temperature = weather_data['main']['temp']
 		details = f"So you are in {city}, {country_code}. You can expect {description} with a temperature of {temperature} degres"
